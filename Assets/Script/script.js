0173111ddc7c37483ff294c0f049df77cee16b16
Christian Colon <christian.colon27@gmail.com>
2021-10-14 01:30:45 -0400

    added function to save search history and refactoring to jquery

--- Assets/Script/script.js
+++ Assets/Script/script.js
@@ -1,34 +1,47 @@
 var apiKey = "27b5c26487b921c1449252a26f2d4cc3";
-var currentEl = document.getElementById("current-container");
-var searchBtnEl = document.getElementById("searchBtn");
-var cityInputEl = document.getElementById("cityInput");
+var currentEl = $("#current-container");
+var searchBtnEl = $("#searchBtn");
+var cityInputEl = $("#cityInput").val().trim();
+var searchHistoryEl = $("#searchHistory")
+var forecastEl = $(".forecast-container")
+
+function handleSearch(e) {
+    if (!cityInputEl) {
+        return;
+    } else {
+        e.preventDefault();
+        grabCity(cityInputEl);
+        searchHistory(cityInputEl);
+        cityInputEl.html("");
+    }
+};
 
 function grabCity(city) {
     var weatherApi = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&appid=" + apiKey;
     fetch(weatherApi)
-    .then(function(res) {
-        return res.json();
-    }).then(function(data) {
-        forecastGrab(data.lat, data.lon)
-    })
+        .then(function (res) {
+            return res.json();
+        }).then(function (data) {
+            forecastGrab(data.lat, data.lon)
+        })
 };
 
 function forecastGrab(lat, lon,) {
-    var forecastApi = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat +"&lon=" + lon +"&appid=" + apiKey;
+    var forecastApi = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey;
     fetch(forecastApi)
+    .then(function (res) {
+        return res.json();
+    })
+    .then(function (data) {
+        console.log(data);
+        weatherData(data);
+    })
 }
 
-function handleSearch(e) {
-    if(!cityInputEl.value) {
-        return;
-    }
-    e.preventDefault();
-    var val = cityInputEl.value.trim();
-    console.log(val);
-    grabCity(val);
-    cityInputEl.value = "";
-};
+function weatherData() {} 
 
+
+function searchHistory() {}
 
 
 searchBtnEl.addEventListener("submit", handleSearch);
